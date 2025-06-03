65740351aee811c5faffc2f9e8a74e5c8271a4cc
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2025-06-03 16:48:11 -0400

    ...github callback fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 router.use('/', require('./swagger'));
 
 router.use('/cars', require('./cars')); //will look for cars.js file under routes
-router.use('/customers', require('./customers')); //will look for customers.js file under routes..
+router.use('/customers', require('./customers')); //will look for customers.js file under routes
 
 //authentication
 router.get('/login', passport.authenticate('github'), (req, res) => {});
